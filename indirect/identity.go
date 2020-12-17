3712c42a79640ac674c9ebefcb5276078cd087de
Danny Hermes <dhermes@blend.com>
2020-12-16 21:23:23 -0600

    Adding some more (unused) code.

--- indirect/identity.go
+++ indirect/identity.go
@@ -5,6 +5,16 @@ func IdentityInt(i int) int {
 	return i
 }
 
+// IdentityInt64 is the identity function for 64 bit integers.
+func IdentityInt64(i int64) int64 {
+	return i
+}
+
+// IdentityFloat32 is the identity function for 32 bit floating point numbers.
+func IdentityFloat32(f float32) float32 {
+	return f
+}
+
 // IdentityFloat64 is the identity function for 64 bit floating point numbers.
 func IdentityFloat64(f float64) float64 {
 	return f
